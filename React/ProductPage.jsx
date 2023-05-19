f5ccc19b6d86e80571f37eb30db72eb034572aa6
서창열 <kongbaguni@gmail.com>
2023-05-19 14:02:06 +0900

    제품 자세히보기 구현

---
+++ React/ProductPage.jsx
@@ -0,0 +1,35 @@
+function ProductPage() {
+    const [data,setData] = React.useState({rating:{rate:0,count:0}, price:0, title: ''});
+    const params = new URLSearchParams(location.search);
+
+    React.useEffect(()=> {
+        reload();
+    },[]);
+
+    function reload() {
+        const id = params.get('id');
+        const url = 'https://fakestoreapi.com/products/' + id
+        axios.get(url).then((res)=>res.data).then((json)=> {
+            setData(json);   
+            console.log(json);         
+        })
+    }
+
+    return (
+        <article>
+            <header><h2>{data.title}</h2></header>
+            <p>
+            <img src={data.image} width={300} height={300} alt={data.title}/>
+            </p>
+            <ul>
+                <li>price : ${data.price}</li>
+                <li>rating : {data.rating.rate} </li>
+                <li>count : {data.rating.count} </li>
+                <li>category : {data.category}</li>
+            </ul>
+            <p>
+                {data.description}
+            </p>
+        </article>
+    )
+}
--- React/components/Product.jsx
+++ React/components/Product.jsx
@@ -1,7 +1,7 @@
 function Product(props) {
     return (
         <div>
-            <h3>{props.data.title}</h3>
+            <h3><a href={"/React/ProductPage.html?id="+props.data.id}>{props.data.title}</a></h3>
             <img src={props.data.image} alt={props.data.title} width = "100px" height = "100px" />
             <ul>
                 <li>${props.data.price}</li>
