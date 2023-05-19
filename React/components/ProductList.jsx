14d524e057904251c96bae0bd7c005c8bb875eb4
서창열 <kongbaguni@gmail.com>
2023-05-19 10:21:28 +0900

    sort 오름차순 내림차순 변경 기능 추가

--- React/components/ProductList.jsx
+++ React/components/ProductList.jsx
@@ -1,23 +1,39 @@
 function ProductList() {
     const [products, setProducts] = React.useState([]);
+    const [sortDesc, setSortDesc] = React.useState(true);
+    const [loadFinish, setLoadFinish] = React.useState(false);
 
     React.useEffect(()=> {
-        if(products.length > 0) {
+        reload();
+    });
+
+    function reload(){
+        if(loadFinish) {
             return;
         }
-        console.log("react loaded!")
-        axios.get('https://fakestoreapi.com/products')
+        console.log("react loaded!");
+        const url = 'https://fakestoreapi.com/products'+ (sortDesc ? '?sort=desc' : '' );
+        console.log("request : " + url);
+        axios.get(url)
         .then((res)=>res.data)
         .then((json)=> {
             console.log(json);
             setProducts(json);
-        })
-    });
+            setLoadFinish(true);
+        });
+    }
+
+    function toggleSort() {
+        setSortDesc(!sortDesc);
+        setLoadFinish(false);
+        reload();
+    }
 
     return (
         <article>
             <header><h2>Product List</h2></header>
             <p>product length : {products.length}</p>
+            <p>Sort : <button onClick={toggleSort}>{sortDesc ? 'DESC' : 'ASC'}</button></p>
             <ol>
                 {products.map(product=> <li key={product.id}><Product data = {product} /></li>)}
             </ol>
