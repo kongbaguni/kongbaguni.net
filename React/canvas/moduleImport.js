d4ccf640c87def320c2e88e85724515937cd1dbd
chang-yul, Seo <kongbaguni@gmail.com>
2023-05-25 12:23:32 +0900

    js 불러오기 로직 수정..

---
+++ React/canvas/moduleImport.js
@@ -0,0 +1,22 @@
+const modules = [
+    './App.jsx',
+    './components/BlendModeSelector.js',
+    './components/CheckBox.js',
+    './components/ColorPicker.js',
+    './components/CommentCreateForm.js',
+    './components/CommentList.js',
+    './components/CommentUpdateForm.js',
+    './components/KCanvasView.js',
+    './components/Navigation.js',
+    './components/People.js',
+    './components/RangePicker.js',
+    './components/ToggleButton.js',
+    './components/VideoPreview.js'
+]
+
+for (let i = 0; i < modules.length; i++) {
+    let script = document.createElement('script');
+    script.src = modules[i];
+    script.type = "text/babel";
+    document.head.appendChild(script);
+}
