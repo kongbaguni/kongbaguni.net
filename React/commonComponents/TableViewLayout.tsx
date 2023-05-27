24e8b4b092a9dfbc863a69adee3982c89d527855
서창열 <kongbaguni@gmail.com>
2023-05-28 08:37:26 +0900

    title 없는 Row 구현

--- React/commonComponents/TableViewLayout.tsx
+++ React/commonComponents/TableViewLayout.tsx
@@ -7,16 +7,30 @@ interface TableViewLayoutProps {
 }
 
 const TableViewLayout = (props:TableViewLayoutProps) => {
+    function makeRow(data:TableViewLayoutRow,id:number) {
+        if(data.title==="") {
+            return (
+                <tr key={id}>
+                    <td colSpan={2}>{data.component}</td>
+                </tr>
+            )
+        } else {
+            return (
+                <tr key={id}>
+                    <th>{data.title}</th>
+                    <td>{data.component}</td>
+                </tr>
+            )
+        }
+    }
     return (
         <table className="table-primary table-striped-columns"> 
             <tbody>
             { 
-                props.datas.map(data => (
-                    <tr key={data.title}>
-                        <th>{data.title}</th>
-                        <td>{data.component}</td>
-                    </tr>
-                ))
+                props.datas.map(data => {
+                    const id = props.datas.indexOf(data);
+                    return makeRow(data,id)
+                })
             }
             </tbody>
         </table>
